8088d669e129d07cf0743d9b3d6fddc49d775627
Suraj lalwani <suraj@nanonets.com>
2025-03-28 16:10:50 +0530

    Updating varstf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -19,9 +19,11 @@ variable "ebs_size" {
 variable "ecr_repo_url" {
   description = "Base ECR repository URL (e.g., 123456789012.dkr.ecr.us-east-1.amazonaws.com)"
   type        = string
+  default = "placeholder-ecr-repo-url"
 }
 
 variable "ecr_image_uri" {
   description = "Full ECR image URI (e.g., 123456789012.dkr.ecr.us-east-1.amazonaws.com/my-flask-app:latest)"
   type        = string
+  default = "placeholder-ecr-image-uri"
 }
